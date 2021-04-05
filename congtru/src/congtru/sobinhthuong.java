d37457ec3b428b1862d4f7e9b70bdc2985770ad5
Dung278 <81941929+Dung278@users.noreply.github.com>
2021-04-05 10:53:38 +0700

    Add files via upload

---
+++ congtru/src/congtru/sobinhthuong.java
@@ -0,0 +1,16 @@
+package congtru;
+import java.util.*;
+public class sobinhthuong {
+	public static int sum(int a,int b) {
+		return a+b;
+	}
+	public static void main(String[] args) {
+		int a,b;
+		Scanner kb = new Scanner(System.in);
+		System.out.print("Nhap a :");
+		a = kb.nextInt();
+		System.out.print("\nNhap b :");
+		b=kb.nextInt();
+		System.out.print("\nSum of " + a +" + "+ b +" = " + sum(a,b));
+	}
+}
